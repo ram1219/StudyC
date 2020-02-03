1824e28d01c24050afa4f7df6ec3c885ddfbf8a1
ram1219 <kimgaram0320@naver.com>
2020-02-03 16:42:06 +0900

    project

---
+++ CProject1/main.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
+#include <string.h>
+
+typedef struct _finddata_t FILE_SEARCH;
+
+void GetfileList(char* path);
+
+typedef struct File_ {
+	char* fa;
+};
+
+
+int main(void)
+{
+	int i = 0;
+	char* path = "D:\\document";
+	
+
+	GetfileList(path);
+
+
+	return EXIT_SUCCESS;
+}
+
+void GetfileList(char* path) {
+
+	long h_file;
+	int i = 0;
+	char search_Path[100];
+	char c = 0;
+
+	FILE_SEARCH file_search;
+
+	struct File_ fi;
+
+
+	sprintf_s(search_Path, 100, "%s\\*.*", path);
+
+	if ((h_file = _findfirst(search_Path, &file_search)) == -1L) {
+		printf("No files in current directory!\n");
+	}
+	else {
+
+		do {		
+			
+			if (strcmp(strrchr(&file_search.name, '.'), ".txt") == 0) {
+				fi.fa = file_search.name;
+
+				sprintf_s(search_Path, 100, "%s\\%s", path, fi.fa);
+				FILE* fp = fopen(search_Path, "rt");
+
+				if (fp == NULL)
+				{
+					puts(" 파일 열기 실패  ! ");
+					return;
+				}
+
+				while (c != EOF)
+				{
+					c = fgetc(fp);
+					putchar(c);
+				}
+
+				fclose(fp);
+				i++;
+			}
+
+		} while (_findnext(h_file, &file_search) == 0);
+
+		_findclose(h_file);
+	}
+}
+
+
