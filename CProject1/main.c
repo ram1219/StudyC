861970658459af77b5b5c9f516f9842cb569644d
ram1219 <kimgaram0320@naver.com>
2020-02-04 17:00:26 +0900

    반복 처리 중간

--- CProject1/main.c
+++ CProject1/main.c
@@ -10,78 +10,102 @@ typedef struct _finddata_t FILE_SEARCH;
 
 void GetfileList(char* path);
 
-typedef struct File_ {
-	char* fa;
-};
-
 
 int main(void)
 {
 	int i = 0;
 	char* path = "D:\\document";
 	
-	
-	while (1)
-	{
-		if (_kbhit())
-		{
-			if (getch() == 'z') 
-				break;
-		}
-
-		GetfileList(path);
-	}
+	GetfileList(path);
 
 
 	return EXIT_SUCCESS;
 }
-
 void GetfileList(char* path) {
 
 	long h_file;
-	int i = 0;
+
+	int icount = 0;
+	char *SaveName[100];
+	char* tmpName;
+
+	int iBool = 0;
+
 	char search_Path[100];
 	char c = 0;
+	char* fa;
 
 	FILE_SEARCH file_search;
 
-	struct File_ fi;
+	while (1)
+	{
+		if (_kbhit())						// 무한루프 종료
+		{
+			if (getch() == 'z')
+				break;
+		}
 
+		sprintf_s(search_Path, 100, "%s\\*.*", path);						// fopen으로 사용할 경로 설정
 
+		if ((h_file = _findfirst(search_Path, &file_search)) == -1L) {		// 파일이 없는 경우
+			printf("No files in current directory!\n");
+		}
 
-	sprintf_s(search_Path, 100, "%s\\*.*", path);
+		else {
+			do {
 
-	if ((h_file = _findfirst(search_Path, &file_search)) == -1L) {
-		printf("No files in current directory!\n");
-	}
-	else {
-		do {					
-			if (strcmp(strrchr(&file_search.name, '.'), ".txt") == 0) {
-				fi.fa = file_search.name;
-				i++;
+				if (strcmp(strrchr(&file_search.name, '.'), ".txt") == 0) {	// txt파일만 골라내기
+					fa = file_search.name;							// 파일 이름 저장
 
-				sprintf_s(search_Path, 100, "%s\\%s", path, fi.fa);
-				FILE* fp = fopen(search_Path, "rt");
+					if (icount == 0) { 
+						SaveName[icount] = fa;
+					}
 
-				if (fp == NULL) {
-					return;
+					for (int i = 0; i < icount; i++) {
+														
+						if (&SaveName[i] == fa) {
+							iBool = 0;
+							break;
+						}
+						else {
+							iBool = 1;
+						}
+							
+					}
+					if (iBool == 1) {
+						SaveName[icount] = fa;
+						iBool = 0;
+					}
+
+					for (int i = 0; i <= icount; i++) {
+						if (i == icount) {
+							sprintf_s(search_Path, 100, "%s\\%s", path, SaveName[i]);
+							FILE* fp = fopen(search_Path, "rt");
+
+							if (fp == NULL) {										// 빈 파일일 경우
+								return;
+							}
+
+							while (c != EOF)
+							{
+								c = fgetc(fp);
+								putchar(c);
+							}
+							fclose(fp);
+
+							c = 0;
+							printf("\n\n");
+							Sleep(1000);
+	
+						}
+					}
+					icount++;
 				}
 
-				while (c != EOF)
-				{
-					c = fgetc(fp);
-					putchar(c);
-				}
+			} while (_findnext(h_file, &file_search) == 0);
 
-				fclose(fp);
-				c = 0;
-				printf("\n\n");
-				Sleep(1000);
-			}
-
-		} while (_findnext(h_file, &file_search) == 0);
-
-		_findclose(h_file);
+			_findclose(h_file);
+		}
 	}
 }
 
